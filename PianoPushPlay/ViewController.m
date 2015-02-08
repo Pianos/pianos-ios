33b16d900c72c91a01364f1fa2147c4d9af39f41
James Stiehl <jstiehl@gs-mbp15-james-2.local>
2015-02-07 17:25:23 -0800

    added request to server for piano locations and cleaned up some of the annotation code

--- PianoPushPlay/ViewController.m
+++ PianoPushPlay/ViewController.m
@@ -25,30 +25,7 @@
     imageArray = [[NSMutableArray alloc] init];
     
     CLLocationCoordinate2D portland = CLLocationCoordinate2DMake(45.5241, -122.676201);
-    CLLocationCoordinate2D pianoStark = CLLocationCoordinate2DMake(45.519877, -122.654898);
-    NSString *starkTitle = @"Stark Piano";
-    UIImage *starkImage = [UIImage imageNamed:@"piano2.jpg"];
     
-    CLLocationCoordinate2D salmonPiano = CLLocationCoordinate2DMake(45.515329, -122.673618);
-    NSString *salmonTitle = @"Salmon Springs Piano";
-    UIImage *salmonImage = [UIImage imageNamed:@"piano3.jpg"];
-    
-    CLLocationCoordinate2D montPiano = CLLocationCoordinate2DMake(45.511464, -122.681496);
-    NSString *montTitle = @"SW 5th Piano";
-    UIImage *montImage = [UIImage imageNamed:@"piano4.jpg"];
-                           
-    CLLocationCoordinate2D artPiano = CLLocationCoordinate2DMake(45.516420, -122.682932);
-    NSString *artTitle = @"SW Madison Piano";
-    UIImage *artImage = [UIImage imageNamed:@"piano5.jpg"];
-                               
-    CLLocationCoordinate2D pioneerPiano = CLLocationCoordinate2DMake(45.519362, -122.679611);
-    NSString *pioneerTitle = @"Pioneer Square Piano";
-    UIImage *pioneerImage = [UIImage imageNamed:@"piano6.jpg"];
-                               
-    CLLocationCoordinate2D burnPiano = CLLocationCoordinate2DMake(45.522925, -122.684079);
-    NSString *burnTitle = @"Burnside Piano";
-    UIImage *burnImage = [UIImage imageNamed:@"piano7.jpg"];
-                               
     MKCoordinateSpan span = MKCoordinateSpanMake(.05, .05);
     
     MKCoordinateRegion region = MKCoordinateRegionMake(portland, span);
@@ -56,12 +33,8 @@
     self.pianoMap.delegate = self;
     
     [self.pianoMap setRegion:region animated:YES];
-    [self showAnnotation:pianoStark title:starkTitle image:starkImage];
-    [self showAnnotation:salmonPiano title:salmonTitle image:salmonImage];
-    [self showAnnotation:montPiano title:montTitle image:montImage];
-    [self showAnnotation:artPiano title:artTitle image:artImage];
-    [self showAnnotation:pioneerPiano title:pioneerTitle image:pioneerImage];
-    [self showAnnotation:burnPiano title:burnTitle image:burnImage];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pianoDataReceived:) name:@"httpDataReceived"  object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -79,22 +52,24 @@
 }
 
 -(void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
-    int i;
-    NSLog(@"%@", [[mapView.annotations objectAtIndex:0] pianoImage]);
-    if ([view.annotation.title  isEqual: @"Stark Piano"]) {
-        i=0;
-    } else if ([view.annotation.title  isEqual: @"Salmon Springs Piano"]){
-        i =1;
-    } else if ([view.annotation.title  isEqual: @"SW 5th Piano"]){
-        i = 2;
-    } else if ([view.annotation.title  isEqual: @"SW Madison Piano"]){
-        i = 3;
-    } else if([view.annotation.title  isEqual: @"Pioneer Square Piano"]){
-        i = 4;
-    } else {
-        i = 5;
-    }
-    self.pictureView.image = [imageArray objectAtIndex:i];
+
+    
+//    int i;
+//    NSLog(@"%@", [[mapView.annotations objectAtIndex:0] pianoImage]);
+//    if ([view.annotation.title  isEqual: @"Stark Piano"]) {
+//        i=0;
+//    } else if ([view.annotation.title  isEqual: @"Salmon Springs Piano"]){
+//        i =1;
+//    } else if ([view.annotation.title  isEqual: @"SW 5th Piano"]){
+//        i = 2;
+//    } else if ([view.annotation.title  isEqual: @"SW Madison Piano"]){
+//        i = 3;
+//    } else if([view.annotation.title  isEqual: @"Pioneer Square Piano"]){
+//        i = 4;
+//    } else {
+//        i = 5;
+//    }
+//    self.pictureView.image = [imageArray objectAtIndex:i];
     
    
     PianoAnnotations *pianoAnnotation = view.annotation;
@@ -113,6 +88,26 @@
 
 -(void)mapView:(MKMapView *)mapView didDeselectAnnotationView:(MKAnnotationView *)view {
     self.pictureView.image = nil;
+}
+
+-(void)pianoDataReceived: (NSNotification *) notification {
+    
+    NSDictionary *json = [notification object];
+    
+    //Place annotations for each piano on map after receiving the data from server
+    for (id key in json){
+        NSDictionary *object = [json objectForKey:key];
+        NSString *title = [object objectForKey:@"title"];
+        NSString *imageName = [object objectForKey:@"image"];
+        NSNumber *lat = [object objectForKey:@"lat"];
+        NSNumber *lon = [object objectForKey:@"lon"];
+        CLLocationCoordinate2D pianoCord = CLLocationCoordinate2DMake(lat.doubleValue, lon.doubleValue);
+        UIImage *image = [UIImage imageNamed:imageName];
+        [self showAnnotation:pianoCord title:title image:image];
+    }
+    
+    
+    
 }
 
 @end
